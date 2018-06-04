4bbb72a8ba1af381903f7c23c48189a193a41fd3
webpatch <webpatch@sina.com>
2018-06-04 23:54:41 +0800

    update

---
+++ app/controller/user.js
@@ -0,0 +1,20 @@
+exports.index = async ctx => {
+  console.log(ctx);
+  ctx.body = 'hello';
+};
+
+
+exports.create = async ctx => {
+  console.log(ctx);
+  ctx.body = 'hello';
+};
+
+exports.update = async ctx => {
+  console.log(ctx);
+  ctx.body = 'hello';
+};
+
+exports.destroy = async ctx => {
+  console.log(ctx);
+  ctx.body = 'hello';
+};
